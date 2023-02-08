cca98d9c0cf258c3c9f13a92eb81cf23582a12e4
Ben Streich <ben.streich@icloud.com>
2023-02-08 12:48:27 +0100

    PizzaKonfig Update

--- Übung 1/Übung 1.5/PizzaKonfig.php
+++ Übung 1/Übung 1.5/PizzaKonfig.php
@@ -8,7 +8,35 @@
 </head>
 <body>
    <h1>Pizza Konfigurator</h1>
+   
+   <form method="POST" action="http://mosermodul.test/%C3%9Cbung%201/%C3%9Cbung%201.5/PizzaKonfig.php">
+   
+      <select name="zutaten">
+      <option value="Zutat auswählen">Zutat auswählen</option>
+      <option value="Salami">Salami</option>
+      <option value="Zwiebeln">Zwiebeln</option>
+      <option value="Speck">Speck</option>
+      <option value="Extra Käse">Extra Käse</option>
 
+   </select> 
+      <input type="submit" value="Hinzufügen">
+   </form> <br>
+
+   <?php
+
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+      $zutat = array($_POST['zutaten']);
+
+      foreach($zutat as $zutaten => $val) {
+         echo "$zutaten = $val<br>";
+       }
+    }
+   ?>
+
+
+   
 
 
 </body>
