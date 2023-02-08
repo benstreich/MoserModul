8bc2f1e75aed1e3a084501e450b26b8c5a8b4359
Ben Streich <ben.streich@icloud.com>
2023-02-08 12:52:11 +0100

    Update PizzaKonfig

--- Übung 1/Übung 1.5/PizzaKonfig.php
+++ Übung 1/Übung 1.5/PizzaKonfig.php
@@ -1,43 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Pizza Ding</title>
-</head>
-<body>
-   <h1>Pizza Konfigurator</h1>
-   
-   <form method="POST" action="http://mosermodul.test/%C3%9Cbung%201/%C3%9Cbung%201.5/PizzaKonfig.php">
-   
-      <select name="zutaten">
-      <option value="Zutat auswählen">Zutat auswählen</option>
-      <option value="Salami">Salami</option>
-      <option value="Zwiebeln">Zwiebeln</option>
-      <option value="Speck">Speck</option>
-      <option value="Extra Käse">Extra Käse</option>
+<?php
+session_start();
 
-   </select> 
-      <input type="submit" value="Hinzufügen">
-   </form> <br>
+$Zutaten = array();
+if (isset($_SESSION['Zutaten'])) {
+    $Zutaten = $_SESSION['Zutaten'];
+}
 
-   <?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $Zutat = $_POST['Zutat'];
+    array_push($Zutaten, $Zutat);
+    $_SESSION['Zutaten'] = $Zutaten;
+}
+?>
 
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-      $zutat = array($_POST['zutaten']);
-
-      foreach($zutat as $zutaten => $val) {
-         echo "$zutaten = $val<br>";
-       }
-    }
-   ?>
+<h1>Pizzakonfigurator</h1>
 
 
-   
+<p>Deine Pizza besteht aus folgenden Zutaten:</p>
 
 
-</body>
-</html>
+<ul>
+    <?php 
+    foreach ($Zutaten as $value) {
+        echo "<li>
+        $value
+        </li>";
+      }
+    ?>
+</ul>
+
+<form method="POST" action="http://m151.test/Uebungen/Uebung1.5/PizzaKonfigurator.php">
+    <input type="text" name="Zutat" placeholder="Zutat" />
+    <input type="submit" value="Absenden"/>
+</form>
