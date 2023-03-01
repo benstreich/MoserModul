f8d6b063b40b3eb39906ecd84df043f71d4fc510
Ben Streich <ben.streich@icloud.com>
2023-03-01 13:47:47 +0100

    Änderungen und so (mir geht es nicht gut) 🗿🪦👽☠️☠️

---
+++ Übung 2/Übung 2.4/kunden.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Neuer Kunde</title>
+</head>
+<body>
+   <form>
+      <label for="firstname">Firstname:</label>
+      <input type="text" name="firstname"> <br>
+      <label for="lastname">Lastname:</label>
+      <input type="text" name="lastname"> <br>
+      <label for="jobtitle">jobtitle</label>
+      <input type="text" name="jobtitle"> <br>
+      <input type="submit">
+
+   </form>
+</body>
+</html>
